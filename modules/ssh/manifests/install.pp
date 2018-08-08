18ec09635a4308ca8950e4c1542b6b20b10c9d7d
Ste Rose <macrosity@gmail.com>
2018-08-08 23:54:34 +0100

    general updates

--- modules/ssh/manifests/install.pp
+++ modules/ssh/manifests/install.pp
@@ -1,5 +1,8 @@
-class ssh::install {
-  package { 'openssh-server':
+class ssh::install (
+  String $package_name = $::ssh::package_name,
+  ) {
+  package { 'arbitrary-sshd-name':
     ensure => present,
+    name   => $package_name,
   }
 }
--- modules/ssh/manifests/service.pp
+++ modules/ssh/manifests/service.pp
@@ -1,6 +1,9 @@
-class ssh::service {
-  service { 'sshd':
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+  ){
+  service { 'arbitrary-sshd':
     ensure     => running,
+    name       => $service_name
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
